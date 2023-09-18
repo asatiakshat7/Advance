577479ee8d336653990ed5387d7f3f3884b5fd57
Akshat Asati <asatiakshat7@gmail.com>
2023-09-18 22:26:29 +0530

    Mailer

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,76 +1,131 @@
 const nodemailer = require("nodemailer");
+const ejs = require('ejs');
 const env=require('./environment');
-const ejs=require("ejs");
-const path=require('path');
-
+const path = require('path')
 
 let transporter = nodemailer.createTransport({
-    service:"gmail",
-    host: "smtp.gmail.com",
+    service: 'gmail',
+    host: 'smtp.gmail.com',
     port: 587,
     secure: false,
+    logger:true,
+    debug:true,
+    secureConnection:false,
     auth: {
-      // TODO: replace `user` and `pass` values from <https://forwardemail.net>
-      user: 'asatiakshat7@gmail.com',
-      pass: '@a200386'
+        user: "asatiakshat7@gmail.com",   //'alchemy.cn18',
+        pass:  "cudm wcta cnab jvpg"
+    },
+    tls:{
+      rejectUnauthorized:true
     }
-  });
+});
 
-  let renderTemplate=(data,relativePath)=>{
-    let mainHtml;
+let renderTemplate = (data, relativePath) => {
+    let mailHTML;
     ejs.renderFile(
-        path.join(__dirname,'../views/mailers',relativePath),
+        path.join(__dirname, '../views/mailers', relativePath),
         data,
-        function(err,template){
-            if(err){
-                console.log('error in render template');
-                return ;
-            }
-            mainHtml=template;
+        function (err, template) {
+            if (err) { console.log('error in rendering template', err); return }
+
+            mailHTML = template;
         }
     )
-    return mainHtml;
-  }
 
-  module.exports={
-    transporter:transporter,
-    renderTemplate:renderTemplate
-  }
+    return mailHTML;
+}
 
+module.exports = {
+    transporter: transporter,
+    renderTemplate: renderTemplate
+}
 // const nodemailer = require("nodemailer");
-// const ejs = require('ejs');
-// const path = require('path')
+// const env=require('./environment');
+// const ejs=require("ejs");
+// const path=require('path');
 
 
 // let transporter = nodemailer.createTransport({
-//     service: 'gmail',
-//     host: 'smtp.gmail.com',
-//     port: 587,
-//     secure: false,
-//     auth: {
-//         user: 'alchemy.cn18',
-//         pass: 'codingninjas'
+//   service: 'gmail',
+//    host: 'smtp.gmail.com',
+//    port: 465,
+//    secure: true,
+//    auth: {
+//     user: 'asatiakshat7@gmail.com',
+//     pass: '@a200386',
+//    },(err, info) => {
+//     if (err) {
+//         console.log('Error in sending mail', err);
+//         return;
 //     }
-// });
+//     return ;
+//   }
+//     // service:"gmail",
+//     // host: "smtp.gmail.com",
+//     // port: 465,
+//     // secure: false,
+//     // auth: {
+//     //   // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+//     //   user: 'asatiakshat7@gmail.com',
+//     //   pass: '@a200386'
+//     // }
+//   });
 
-
-// let renderTemplate = (data, relativePath) => {
-//     let mailHTML;
+//   let renderTemplate=(data,relativePath)=>{
+//     let mainHtml;
 //     ejs.renderFile(
-//         path.join(__dirname, '../views/mailers', relativePath),
+//         path.join(__dirname,'../views/mailers',relativePath),
 //         data,
-//         function(err, template){
-//          if (err){console.log('error in rendering template', err); return}
-         
-//          mailHTML = template;
+//         function(err,template){
+//             if(err){
+//                 console.log('error in render template');
+//                 return ;
+//             }
+//             mainHtml=template;
 //         }
 //     )
+//     return mainHtml;
+//   }
 
-//     return mailHTML;
-// }
+//   module.exports={
+//     transporter:transporter,
+//     renderTemplate:renderTemplate
+//   }
+
+// // const nodemailer = require("nodemailer");
+// // const ejs = require('ejs');
+// // const path = require('path')
 
 
-// module.exports = {
-//     transporter: transporter,
-//     renderTemplate: renderTemplate
-// }
+// // let transporter = nodemailer.createTransport({
+// //     service: 'gmail',
+// //     host: 'smtp.gmail.com',
+// //     port: 587,
+// //     secure: false,
+// //     auth: {
+// //         user: 'alchemy.cn18',
+// //         pass: 'codingninjas'
+// //     }
+// // });
+
+
+// // let renderTemplate = (data, relativePath) => {
+// //     let mailHTML;
+// //     ejs.renderFile(
+// //         path.join(__dirname, '../views/mailers', relativePath),
+// //         data,
+// //         function(err, template){
+// //          if (err){console.log('error in rendering template', err); return}
+         
+// //          mailHTML = template;
+// //         }
+// //     )
+
+// //     return mailHTML;
+// // }
+
+
+// // module.exports = {
+// //     transporter: transporter,
+// //     renderTemplate: renderTemplate
+// // }
--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -1,22 +1,45 @@
 const nodeMailer = require('../config/nodemailer');
 
-
 // this is another way of exporting a method
 exports.newComment = (comment) => {
-    let htmlString = nodeMailer.renderTemplate({comment: comment}, '/comments/new_comment.ejs');
+    // console.log('inside newComment mailer', comment);
+    let htmlString = nodeMailer.renderTemplate({ comment: comment }, '/comments/new_comment.ejs');
 
     nodeMailer.transporter.sendMail({
-       from: 'wwe@gmail.com',
-       to: comment.user.email,
-       subject: "New Comment Published!",
-       html: htmlString
+        from: 'asatiakshat7@gmail.com',
+        to: comment.user.email,
+        subject: "New Comment Published!",
+        html: htmlString  //'<h1>Yup, your comment is now published!</h1>'
     }, (err, info) => {
-        if (err){
+        if (err) {
             console.log('Error in sending mail', err);
             return;
         }
 
-        console.log('Message sent', info);
+       console.log('Message sent', info);
         return;
     });
 }
+// const nodeMailer = require('../config/nodemailer');
+
+
+// // this is another way of exporting a method
+// exports.newComment = (comment) => {
+//     let htmlString = nodeMailer.renderTemplate({comment: comment}, '/comments/new_comment.ejs');
+
+//     nodeMailer.transporter.sendMail({
+//        from: 'asatiakshat7@gmail.com',
+//        to: comment.user.email,
+//        subject: "New Comment Published!",
+//        html: htmlString
+//     });
+//     //  (err, info) => {
+//     //     if (err){
+//     //         console.log('Error in sending mail', err);
+//     //         return;
+//     //     }
+
+//     //     console.log('Message sent', info);
+//     //     return;
+//     // });
+// }
